114e32c94fbd50aedb15719fcc876750d1b84c5d
rdburman2205 <rdburman2205@gmail.com>
2019-10-20 02:34:31 +0530

    homepage added

---
+++ mae/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MaeConfig(AppConfig):
+    name = 'mae'
--- monitoringandevaluation/settings.py
+++ monitoringandevaluation/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mae.apps.MaeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
