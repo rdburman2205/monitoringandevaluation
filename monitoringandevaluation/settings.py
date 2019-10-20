f9dd0da8ecd32557058ad311b649e8b7d3408d17
rdburman2205 <rdburman2205@gmail.com>
2019-10-20 16:19:10 +0530

    homepage added

--- monitoringandevaluation/settings.py
+++ monitoringandevaluation/settings.py
@@ -14,8 +14,10 @@ import os
 import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
